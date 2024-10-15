4d602e5ac0a51fe55429deb0396c6b54ed289443
Mark Jin <mark@pixmoving.net>
2024-10-15 11:57:23 +0800

    feat: use subplot to visualize CPU LOAD, MEMORY USAGE, CPU TEMPERATURE in the same frame

--- scripts/load_monitor_visualization.py
+++ scripts/load_monitor_visualization.py
@@ -12,13 +12,26 @@ if __name__ == '__main__':
     z = []
     cpu = []
     mem = []
+    cpu_temp = []
     for row in csv_reader:
       x.append(float(row[0]))
       y.append(float(row[1]))
       z.append(float(row[2]))
       cpu.append(float(row[3]))
       mem.append(float(row[4]))
-  plt.scatter(np.array(x), np.array(y), c=np.array(cpu))
-  plt.colorbar(label='Cpu Load')
+      cpu_temp.append(float(row[5]))
+  
+  fig, axs = plt.subplots(3)
+  fig.suptitle("CPU MEM MONITOR")
+  axs[0].set_title("CPU LOAD")
+  img_cpu_load = axs[0].scatter(np.array(x), np.array(y), c=np.array(cpu))
+  axs[1].set_title("MEMORY USAGE")
+  img_mem_usage = axs[1].scatter(np.array(x), np.array(y), c=np.array(mem))
+  axs[2].set_title("CPU TEMPERATURE")
+  img_cpu_temp = axs[2].scatter(np.array(x), np.array(y), c=np.array(cpu_temp))
+  plt.colorbar(img_cpu_load, label='Cpu Load', ax=axs[0])
+  plt.colorbar(img_mem_usage, label='Mem Usage', ax=axs[1])
+  plt.colorbar(img_cpu_temp, label='Cpu Temp', ax=axs[2])
+
   plt.show()
   
